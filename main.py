79fb0d959e8f6708a4d0692f8e24351e03a3a374
MadisonAngel <maddiotomboy@yahoo.com>
2021-09-10 22:35:27 -0700

    adding logic outline

---
+++ main.py
@@ -0,0 +1,7 @@
+# logic of the code
+# ask for their name
+# choose a difficultly 
+# start game
+    # if win: ask if they want to continue
+    # else lost calculate score and if high score add them to database of winners
+    # ask if they want to try again
