cfb314c4a481add048c657b8799a221d3d23f055
sarebear54 <90652236+sarebear54@users.noreply.github.com>
2021-12-12 22:15:09 -0800

    Add files via upload

--- game_gui.py
+++ game_gui.py
@@ -39,12 +39,12 @@ class Game_Screen(tk.Frame):
 
         tk.Button(self, text="Menu", font=("Georgia", 10, "bold"), bg = "skyblue4", fg = "white", command=lambda: self.back_to_user(), width=10).grid(row = 0, column = 0, columnspan=2)
         self.imgLabel.grid(row=1,column=0,columnspan=3, padx=10, pady=40)
-        Label(self, textvariable=self.lblWord, font=("Georgia", 14), bg = "white", fg = "black").grid(row=1,column=3,columnspan=6, padx=10)
+        Label(self, textvariable=self.lblWord, font=("Georgia", 14), bg = "lightcyan3", fg = "black").grid(row=1,column=3,columnspan=6, padx=10)
         n=0
         for c in ascii_uppercase:
             Button(self, text=c, font=("Georgia", 10, "bold"), bg = "white", fg = "black", command=lambda c=c: self.guess(c), width=5).grid(row=2+n//9, column=n%9, sticky = 'WE', padx=2, pady=2)
             n+=1
-        Button(self, text="Phrase", font=("Georgia", 10, "bold"), bg = "skyblue4", fg = "white", command=lambda: self.guess_phrase(), width=5).grid(row=4, column=9,sticky="NSWE")
+        Button(self, text="Guess", font=("Georgia", 10, "bold"), bg = "skyblue4", fg = "white", command=lambda: self.guess_phrase(), width=5).grid(row=4, column=9,sticky="NSWE")
         Button(self, text="New", font=("Georgia", 10, "bold"), bg = "skyblue4", fg = "white", command=lambda: self.start_new_game(), width=5).grid(row=2, column=9,sticky="NSWE")
 
     def guess(self,letter):
@@ -93,7 +93,7 @@ class Game_Screen(tk.Frame):
 
         if not check_if_highscore_exists(game_data.game_mode, p) and p!=0:
             add_high_score(game_data.game_mode, user_data.user_name, p)
-            if check_top_scores(game_data.game_mode, user_data.user_name, p):
+            if check_top_scores(game_data.game_mode, user_data.user_name, p):   
                 messagebox.showwarning("Hangman", "New High Score!")
         update__user_data_score(user_data.user_name, game_data.game_mode, 0)
 
@@ -111,6 +111,7 @@ class Phrase_Screen(tk.Frame):
         ,PhotoImage(file="hangman_images/hang1.png"),PhotoImage(file="hangman_images/hang0.png")]
         tk.Frame.__init__(self, parent)
         self.controller = controller
+        self.config(bg="lightcyan3")        
         self.lblWord=StringVar()
         self.gue=StringVar()
     def set_hangman(self, hangman: Hangman):
@@ -119,10 +120,11 @@ class Phrase_Screen(tk.Frame):
         self.imgLabel = img
     def set_up_screen(self):
         self.lblWord.set(self.hangman.string_completed())
-        tk.Button(self, text="Back", command=lambda: self.controller.show_frame("Game_Screen")).grid(row = 0, column = 0)
-        Label(self, textvariable=self.lblWord).grid(row = 1, column = 0, columnspan=6, padx=10)
-        Entry(self, textvariable=self.gue).grid(row = 2, column = 0, columnspan=6, padx=10)
-        Button(self, text='Guess', command=lambda:self.guess(), width = 5).grid(row = 3, column = 0)
+        Label(self, textvariable=self.lblWord, font=("Georgia", 14, "bold"), bg = "lightcyan3", fg = "black").pack(pady = 50)
+        Entry(self, textvariable=self.gue).pack(pady=5)
+        Button(self, text='Guess', command=lambda:self.guess(), font=("Georgia", 10, "bold"), bg = "skyblue4", fg = "white", width = 10, height=1).pack(pady=5)
+        tk.Button(self, text="Back", command=lambda: self.controller.show_frame("Game_Screen"), font=("Georgia", 10, "bold"), bg = "skyblue4", fg = "white", width = 10).pack(pady=5)
+
 
     def guess(self):
         if(self.hangman.guess_phrase(self.gue.get().upper()) is False):
@@ -147,7 +149,7 @@ def game_over_screen():
     all_data = get_user_data(user_data.user_name)
     scores = json.loads(all_data[2])
     p = scores[game_data.game_mode]
-    Label(top, text=f"Points: {p}").pack()
-    Label(top, text="Game Over").pack()
-    Button(top, text="New_Game", command=lambda: screens.screen_data["Game_Screen"].new_game()).pack()
-    Button(top, text="Back", command=lambda: screens.screen_data["Game_Screen"].end_game()).pack()
+    Label(top, text="Game Over", font=("Georgia", 20, "bold"), fg = "skyblue4",).pack(pady=5)
+    Label(top, text=f"Points Earned: {p}", font=("Georgia", 14, "bold"), fg = "black", pady=20).pack(pady=5)
+    Button(top, text="New_Game", command=lambda: screens.screen_data["Game_Screen"].new_game(), font=("Georgia", 10, "bold"), bg = "skyblue4", fg = "white").pack(pady=5)
+    Button(top, text="Back", command=lambda: screens.screen_data["Game_Screen"].end_game(), font=("Georgia", 10, "bold"), bg = "skyblue4", fg = "white").pack(pady=5)
